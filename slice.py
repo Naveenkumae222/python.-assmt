c103e4439133b90b95142630f92dcd6b2d92ba49
Naveenkumae222 <naveenpokkili.marolix@gmail.com>
2024-02-01 18:20:04 +0530

    chinna

---
+++ slice.py
@@ -0,0 +1,27 @@
+k="a medical condition of the thyroid"
+print(k[0:9])       #a meical
+print(k[3:18])      #edical conditio
+print(k[7:25:2])    #a odto ft
+print(k[0:25:3])    #aec ntnfh
+print(k[3:8:5])     #e
+print(k[-33:-14])   # medical condition
+print(k[-33:-14:2]) # eia odto
+print(k[-34:-10:5]) #aicto
+print(k[-25:-9])    # condition of th
+print(k[-15:34])    # of the thyroid
+print(k[2:-5])      #mediccal condition of the th
+print(k[0:-25])     #a medical
+print(k[-34:-3:4])  #adlniohh
+print(k[0:-25:4])   #adl
+print(k[10:-8:2])   #cniio h
+print(k[7:-1])      #al condition of the thyroi
+print(k[-34:-12:2]) #amdclciino
+
+
+
+# "the cyber city of hyd"
+s= "the cyber city of hyd"
+a=len(s)
+for i in range(a):
+    print("the char present in the +ve index is",i,"is",s[i],"and -ve index is",i-a)
+
