871c758307c8dd2abda9bbe209f93a7f59fdc6b4
naveenkumar222 <156795709+Naveenkumae222@users.noreply.github.com>
2024-02-01 18:23:03 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+
+name= ""
+password= ""
+
+while name!="shiva" or password !=12345:
+      
+      name=input("enter your name: ")
+      password=int(input("enter your passwoed: "))
+
+print("hello shiva thank you for verification") 
