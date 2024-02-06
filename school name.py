55e5b88a42ae5583b56d8df2816d9ae5f60ca07e
naveenkumar222 <156795709+Naveenkumae222@users.noreply.github.com>
2024-02-06 16:51:15 +0530

    Add files via upload

---
+++ school name.py
@@ -0,0 +1,18 @@
+
+name= input("enter school name to check availabity ")
+if name=="reddy school":
+    print(name," is available ")
+elif name.lower()=="sadhana school":
+    print(name," is available")
+elif name.lower()=="swathi school":
+    print(name," is available")
+elif name.lower()=="gowtham school":
+    print(name," is available")
+else:
+    print(name,"sorry is not available,please try another school")
+
+
+
+
+
+ 
