948c8f31c2e696d54b1a9c5cf59ca7ef29c6cfca
naveenkumar222 <156795709+Naveenkumae222@users.noreply.github.com>
2024-02-06 16:50:35 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+l =[12,34,56,72,56]
+x = l.append(90)
+print(x)
+print(l)   #[12,34,56,90]
+
+
+
+l = [20,30,70,56,89,]
+x = l.append(120)
+y = l.append(160)
+z = l.append(11)
+print(x)
+print(y)
+print(z) 
+print(l)#[20,30,70,56,89,120,160,11]
+
+
